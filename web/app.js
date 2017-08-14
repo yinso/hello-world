950b3c1a52fb7f0c729f9e74551eeee82bd8e337
thelionthatiam <thelionthatiam@users.noreply.github.com>
2017-08-14 15:49:21 -0700

    added some console.log events and used express rather than HTTP call

--- web/app.js
+++ web/app.js
@@ -1,37 +1,11 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-http.createServer(function (req, res) {
-	fs.readFile('button.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
-		res.end();
-	});
-}).listen(8000);
+app.get('/', function (req, res) {
+  res.sendFile('/Users/juliantheberge/Documents/GitHub/hello-world/web/button.html');
+});
 
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(8000, function () {
-//   console.log('Test that this is the new script running on 8000');
-// });
-
-
-/*----------------OLD STUFF----------------*/
-
-
-//HELLO WORLD
-// var express = require('express');
-// var app = express();
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-// app.listen(8000, function () {
-//   console.log('Hello App listening on port 8000!');
-// });
+app.listen(8000, function () {
+  console.log('Hello App listening on port 8000!');
+});
