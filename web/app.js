db2b84e0c5cf7adb2e1481ff15f3e6f36c4208d2
thelionthatiam <thelionthatiam@users.noreply.github.com>
2017-08-15 09:09:32 -0700

    using express.static to serve files now

--- web/app.js
+++ web/app.js
@@ -2,10 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendFile('../web/button.html');
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(8000, function () {
-  console.log('Hello App listening on port 8000!');
+  console.log('Buttons show up on port 8000!');
 });
