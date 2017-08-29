5d2aacb20d5519476b3f28a301e346255b34fb39
Yinso Chen <yinso.chen@gmail.com>
2017-08-29 13:56:12 -0700

    correct order of async calls

--- web/app.js
+++ web/app.js
@@ -20,7 +20,7 @@ var client = new Client({
 	host: 'localhost',
 	database: 'newdatabase',
 	password: 'Mapex133',
-	port: 5432,	
+	port: 5432,
 })
 
 //connecting server to database
@@ -38,29 +38,37 @@ app.listen(8000, function () {
 app.use(bodyParser());
 
 //function for parameterized query
-function basicQuery(t, v) {
+function basicQuery(t, v, cb) {
+	console.log('step 2 - basicQuery', t, v);
 	var text = t;
 	var values = v;
 
 	client.query(text, values, (err, res) => {
+		console.log('step 3 - call back from client query', err, res);
 	  if (err) {
-	    console.log(err.stack)
+			cb(err);
 	  } else {
-	  	console.log(res);
-	    // console.log(res.rows[0])
+			cb(null, res);
 	  }
 	})
 }
 
 //post button which 1) takes in a string 2) posts the string in html 3) prints in the browser console 4) prints in terminal and 5) adds an id to a table
 app.post('/', function (req,res) {
+	console.log('step 1 - post handler', req.body);
 	var obj = req.body;
 	var text = 'INSERT INTO users(user_name, first_name, last_name, email, phone_number) VALUES($1, $2, $3, $4, $5) RETURNING *';
 	var values = [obj.userName, obj.firstName, obj.lastName, obj.email, obj.phone ];
-	basicQuery(text, values);
-
-	console.log(values);
-	res.end();
+	basicQuery(text, values, (err, result) => {
+		console.log('step 4 - callback from basicQuery, return to user', err, result);
+		if (err) {
+			// notify user of the error.
+			res.json(err);
+		} else {
+			// good here. notify user.
+			res.json({ status: 'OK'})
+		}
+	});
 })
 
 
